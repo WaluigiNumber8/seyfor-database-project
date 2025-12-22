662d0abbc1253602f2bac238e8fd1c73bd304d0a
Jan Kunetka <jankunetka@seznam.cz>
2025-12-22 13:19:52 +0100

    Design basic Add Equipment window

---
+++ SeyforDatabaseProject.View/Views/EquipmentAddWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace SeyforDatabaseProject.Views
+{
+    public partial class EquipmentAddWindow : Window
+    {
+        public EquipmentAddWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
