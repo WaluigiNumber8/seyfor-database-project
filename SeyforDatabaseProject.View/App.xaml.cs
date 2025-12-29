54318f1d5712c32b88a5194a783a8aa96cffad5e
Jan Kunetka <jankunetka@seznam.cz>
2025-12-29 17:20:46 +0100

    Refactor App.xaml into using a Host provider

--- SeyforDatabaseProject.View/App.xaml.cs
+++ SeyforDatabaseProject.View/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using SeyforDatabaseProject.Model;
 using SeyforDatabaseProject.Model.Departments;
 using SeyforDatabaseProject.Model.Services;
@@ -14,39 +16,56 @@ namespace SeyforDatabaseProject
     public partial class App : Application
     {
         private const string ConnectionString = "Data Source=SeyforDatabaseDB.db";
-
-        private readonly NavigationStore _navigationStore;
-        private readonly HotelStore _hotelStore;
+        private readonly IHost _host;
 
         public App()
         {
-            _navigationStore = new NavigationStore();
-            DatabaseContextFactory contextFactory = new(ConnectionString);
-            EquipmentDepartment equipmentDepartment = new(new DatabaseDataProvider(contextFactory), new DatabaseDataCreator(contextFactory), new DatabaseValidator(contextFactory));
-            Hotel hotel = new(equipmentDepartment);
-            _hotelStore = new(hotel);
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton(new DatabaseContextFactory(ConnectionString));
+                    services.AddSingleton<IServiceDataCreator, DatabaseDataCreator>();
+                    services.AddSingleton<IServiceDataProvider, DatabaseDataProvider>();
+                    services.AddSingleton<IServiceDataValidator, DatabaseValidator>();
+                    services.AddTransient<EquipmentDepartment>();
+
+                    services.AddSingleton<Hotel>();
+                    services.AddSingleton<HotelStore>();
+                    services.AddSingleton<NavigationStore>();
+
+                    services.AddTransient<EquipmentListingVM>();
+                    services.AddSingleton<Func<EquipmentEditVM>>(s => s.GetRequiredService<EquipmentEditVM>);
+                    services.AddSingleton<NavigationService<EquipmentListingVM>>();
+                    
+                    services.AddTransient<EquipmentEditVM>();
+                    services.AddSingleton<Func<EquipmentListingVM>>(s => s.GetRequiredService<EquipmentListingVM>);
+                    services.AddSingleton<NavigationService<EquipmentEditVM>>();
+                    
+                    services.AddSingleton<MainVM>();
+                    services.AddSingleton(s =>
+                    {
+                        MainWindow mainWindow = new()
+                        {
+                            DataContext = s.GetRequiredService<MainVM>()
+                        };
+                        return mainWindow;
+                    });
+                })
+                .Build();
         }
-        
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            //TODO: Refactor into Dependency Injection
-            _navigationStore.CurrentVM = CreateEquipmentTableVM();
-            
-            MainWindow m = new();
-            m.DataContext = new MainVM(_navigationStore);
-            m.Show();
-        
+            _host.Services.GetRequiredService<NavigationService<EquipmentListingVM>>().Navigate();
+            _host.Services.GetRequiredService<MainWindow>().Show();
+
             base.OnStartup(e);
         }
-        
-        private EquipmentListingVM CreateEquipmentTableVM()
+
+        protected override void OnExit(ExitEventArgs e)
         {
-            return new EquipmentListingVM(_hotelStore, new NavigationService<EquipmentEditVM>(_navigationStore, CreateEquipmentEditVM));
-        }
-        
-        private EquipmentEditVM CreateEquipmentEditVM()
-        {
-            return new EquipmentEditVM(_hotelStore, new NavigationService<EquipmentListingVM>(_navigationStore, CreateEquipmentTableVM));
+            _host.Dispose();
+            base.OnExit(e);
         }
     }
 }
