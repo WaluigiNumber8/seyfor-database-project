8551858f68faae7995243a97297e8e87c6b2d27c
Jan Kunetka <jankunetka@seznam.cz>
2026-01-02 12:58:55 +0100

    Add navigation to header row buttons

--- SeyforDatabaseProject.ViewModel/VMs/MainVM.cs
+++ SeyforDatabaseProject.ViewModel/VMs/MainVM.cs
@@ -1,5 +1,8 @@
-﻿using SeyforDatabaseProject.ViewModel.Core;
+﻿using System.Windows.Input;
+using SeyforDatabaseProject.ViewModel.Core;
+using SeyforDatabaseProject.ViewModel.Equipment;
 using SeyforDatabaseProject.ViewModel.Navigation;
+using SeyforDatabaseProject.ViewModel.Rooms;
 
 namespace SeyforDatabaseProject.ViewModel
 {
@@ -8,14 +11,24 @@ namespace SeyforDatabaseProject.ViewModel
     /// </summary>
     public class MainVM : ViewModelBase
     {
+        #region Commands
+
+        public ICommand NavigateEquipmentCommand { get; }
+        public ICommand NavigateRoomsCommand { get; }
+
+        #endregion
+        
         private readonly NavigationStore _navigationStore;
         
         public ViewModelBase CurrentVM { get => _navigationStore.CurrentVM; }
 
-        public MainVM(NavigationStore navigationStore)
+        public MainVM(NavigationStore navigationStore, NavigationService<ScreenEquipmentOperationsVM> navigateToEquipment, NavigationService<ScreenRoomOperationsVM> navigateToRooms)
         {
             _navigationStore = navigationStore;
             _navigationStore.OnViewModelChanged += WhenChangeViewModel;
+
+            NavigateEquipmentCommand = new NavigateCommand(navigateToEquipment);
+            NavigateRoomsCommand = new NavigateCommand(navigateToRooms);
         }
 
         private void WhenChangeViewModel() => OnPropertyChanged(nameof(CurrentVM));
