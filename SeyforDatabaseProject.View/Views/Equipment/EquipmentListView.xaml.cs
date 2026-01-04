0852d991ce1408f4cdcb0a244aa38286c1815b77
Jan Kunetka <jankunetka@seznam.cz>
2026-01-04 17:42:30 +0100

    Add searchbox to equipment screen

--- SeyforDatabaseProject.View/Views/Equipment/EquipmentListView.xaml.cs
+++ SeyforDatabaseProject.View/Views/Equipment/EquipmentListView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using SeyforDatabaseProject.ViewModel.Equipment;
 
 namespace SeyforDatabaseProject.Views.Equipment
 {
@@ -8,5 +11,28 @@ namespace SeyforDatabaseProject.Views.Equipment
         {
             InitializeComponent();
         }
+
+        private void EquipmentListView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (EquipmentItemsListView == null) return;
+            CollectionViewSource.GetDefaultView(EquipmentItemsListView.ItemsSource).Filter += FilterEquipment;
+        }
+
+        private void FilterBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (EquipmentItemsListView == null) return;
+            CollectionViewSource.GetDefaultView(EquipmentItemsListView.ItemsSource).Refresh();
+        }
+        
+        private bool FilterEquipment(object item)
+        {
+            if (string.IsNullOrEmpty(FilterBox.Text)) return true;
+
+            EquipmentItemVM equipment = (EquipmentItemVM) item;
+            
+            return (equipment.Title.StartsWith(FilterBox.Text, StringComparison.OrdinalIgnoreCase)
+                    || equipment.Description.StartsWith(FilterBox.Text, StringComparison.OrdinalIgnoreCase));
+        }
+        
     }
 }
